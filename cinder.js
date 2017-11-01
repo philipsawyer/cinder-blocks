53d0b80429ba4a0f705e94f706bfb88690d967f9
Philip Sawyer <philsawyer94@gmail.com>
2017-10-31 21:18:45 -0400

    Condensed all small code

--- cinder.js
+++ cinder.js
@@ -137,178 +137,27 @@ function resizeview() {
   if (mediumthreshhold > viewportwidth) {
 
     $(".s0").css("width", 0);
-    $(".s1").css("width", percent1);
-    $(".s2").css("width", percent2);
-    $(".s3").css("width", percent3);
-    $(".s4").css("width", percent4);
-    $(".s5").css("width", percent5);
-    $(".s6").css("width", percent6);
-    $(".s7").css("width", percent7);
-    $(".s8").css("width", percent8);
-    $(".s9").css("width", percent9);
-    $(".s10").css("width", percent10);
-    $(".s11").css("width", percent11);
-    $(".s12").css("width", percent12);
-    $(".s13").css("width", percent13);
-    $(".s14").css("width", percent14);
-    $(".s15").css("width", percent15);
-    $(".s16").css("width", percent16);
-    $(".s17").css("width", percent17);
-    $(".s18").css("width", percent18);
-    $(".s19").css("width", percent19);
-    $(".s20").css("width", percent20);
-    $(".s21").css("width", percent21);
-    $(".s22").css("width", percent22);
-    $(".s23").css("width", percent23);
-    $(".s24").css("width", percent24);
+    for (i = 0; i < 24; i++) {
+      var element = ".s" + (i + 1);
+      $(element).css("width", percents[i]);
+    }
 
-    var s1cmargin = (viewportwidth - percent1) / 2;
-    var s2cmargin = (viewportwidth - percent2) / 2;
-    var s3cmargin = (viewportwidth - percent3) / 2;
-    var s4cmargin = (viewportwidth - percent4) / 2;
-    var s5cmargin = (viewportwidth - percent5) / 2;
-    var s6cmargin = (viewportwidth - percent6) / 2;
-    var s7cmargin = (viewportwidth - percent7) / 2;
-    var s8cmargin = (viewportwidth - percent8) / 2;
-    var s9cmargin = (viewportwidth - percent9) / 2;
-    var s10cmargin = (viewportwidth - percent10) / 2;
-    var s11cmargin = (viewportwidth - percent11) / 2;
-    var s12cmargin = (viewportwidth - percent12) / 2;
-    var s13cmargin = (viewportwidth - percent13) / 2;
-    var s14cmargin = (viewportwidth - percent14) / 2;
-    var s15cmargin = (viewportwidth - percent15) / 2;
-    var s16cmargin = (viewportwidth - percent16) / 2;
-    var s17cmargin = (viewportwidth - percent17) / 2;
-    var s18cmargin = (viewportwidth - percent18) / 2;
-    var s19cmargin = (viewportwidth - percent19) / 2;
-    var s20cmargin = (viewportwidth - percent20) / 2;
-    var s21cmargin = (viewportwidth - percent21) / 2;
-    var s22cmargin = (viewportwidth - percent22) / 2;
-    var s23cmargin = (viewportwidth - percent23) / 2;
-    var s24cmargin = (viewportwidth - percent24) / 2;
-
+    var margins = [];
+    for (i = 0; i < 25; i++) {
+      var value = (viewportwidth - percents[i]) / 2;
+      margins.push(value);
+    }
+    
     $(".s0c").css({
       "width": 0
     });
-    $(".s1c").css({
-      "width": percent1,
-      "margin-right": s1cmargin,
-      "margin-left": s1cmargin
-    });
-    $(".s2c").css({
-      "width": percent2,
-      "margin-right": s2cmargin,
-      "margin-left": s2cmargin
-    });
-    $(".s3c").css({
-      "width": percent3,
-      "margin-right": s3cmargin,
-      "margin-left": s3cmargin
-    });
-    $(".s4c").css({
-      "width": percent4,
-      "margin-right": s4cmargin,
-      "margin-left": s4cmargin
-    });
-    $(".s5c").css({
-      "width": percent5,
-      "margin-right": s5cmargin,
-      "margin-left": s5cmargin
-    });
-    $(".s6c").css({
-      "width": percent6,
-      "margin-right": s6cmargin,
-      "margin-left": s6cmargin
-    });
-    $(".s7c").css({
-      "width": percent7,
-      "margin-right": s7cmargin,
-      "margin-left": s7cmargin
-    });
-    $(".s8c").css({
-      "width": percent8,
-      "margin-right": s8cmargin,
-      "margin-left": s8cmargin
-    });
-    $(".s9c").css({
-      "width": percent9,
-      "margin-right": s9cmargin,
-      "margin-left": s9cmargin
-    });
-    $(".s10c").css({
-      "width": percent10,
-      "margin-right": s10cmargin,
-      "margin-left": s10cmargin
-    });
-    $(".s11c").css({
-      "width": percent11,
-      "margin-right": s11cmargin,
-      "margin-left": s11cmargin
-    });
-    $(".s12c").css({
-      "width": percent12,
-      "margin-right": s12cmargin,
-      "margin-left": s12cmargin
-    });
-    $(".s13c").css({
-      "width": percent13,
-      "margin-right": s13cmargin,
-      "margin-left": s13cmargin
-    });
-    $(".s14c").css({
-      "width": percent14,
-      "margin-right": s14cmargin,
-      "margin-left": s14cmargin
-    });
-    $(".s15c").css({
-      "width": percent15,
-      "margin-right": s15cmargin,
-      "margin-left": s15cmargin
-    });
-    $(".s16c").css({
-      "width": percent16,
-      "margin-right": s16cmargin,
-      "margin-left": s16cmargin
-    });
-    $(".s17c").css({
-      "width": percent17,
-      "margin-right": s17cmargin,
-      "margin-left": s17cmargin
-    });
-    $(".s18c").css({
-      "width": percent18,
-      "margin-right": s18cmargin,
-      "margin-left": s18cmargin
-    });
-    $(".s19c").css({
-      "width": percent19,
-      "margin-right": s19cmargin,
-      "margin-left": s19cmargin
-    });
-    $(".s20c").css({
-      "width": percent20,
-      "margin-right": s20cmargin,
-      "margin-left": s20cmargin
-    });
-    $(".s21c").css({
-      "width": percent21,
-      "margin-right": s21cmargin,
-      "margin-left": s21cmargin
-    });
-    $(".s22c").css({
-      "width": percent22,
-      "margin-right": s22cmargin,
-      "margin-left": s22cmargin
-    });
-    $(".s23c").css({
-      "width": percent23,
-      "margin-right": s23cmargin,
-      "margin-left": s23cmargin
-    });
-    $(".s24c").css({
-      "width": percent24,
-      "margin-right": s24cmargin,
-      "margin-left": s24cmargin
-    });
+    for (i = 0; i < 24; i++) {
+      var element = ".s" + (i + 1) + "c";
+      $(element).css({
+        "width": percents[i],
+        "margin-right": margins[i],
+        "margin-left": margins[i]
+      });
+    }
   }
 };
